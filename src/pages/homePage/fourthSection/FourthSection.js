47e2917234555bc271566dcbea7503294c6dc6cb
im-adnangujjar <adnan.yaqoob.gujar@gmail.com>
2022-03-31 08:37:44 +0100

    card & image

--- src/pages/homePage/fourthSection/FourthSection.js
+++ src/pages/homePage/fourthSection/FourthSection.js
@@ -8,18 +8,16 @@ function FourthSection() {
   return (
     <div>
       <div className={styles.maiContainer}>
-        {/* <Grid container spacing={4}> */}
-        {/* <Grid item lg={6}> */}
+      
         <div className={styles.display}>
-        <div className={styles.image}><Image className={styles.size} src={image} alt="errors" /></div>
-        {/* <Grid item lg={6}> */}
-        <div><p className={styles.paragraph}>Take the next step toward your personal and professional goals with Coursera.</p>
-          <p className={styles.paragraph1}>Join now to receive personalized recommendations from the full Coursera catalog.</p>
-          <div className={styles.button}><p className={styles.buttonStyle}>join for free</p></div>
+          <div className={styles.image}><Image className={styles.size} src={image} alt="errors" /></div>
+
+          <div><p className={styles.paragraph}>Take the next step toward your personal and professional goals with Coursera.</p>
+            <p className={styles.paragraph1}>Join now to receive personalized recommendations from the full Coursera catalog.</p>
+            <div className={styles.button}><p className={styles.buttonStyle}>join for free</p></div>
+          </div>
         </div>
-        </div>
-        {/* </Grid> */}
-        {/* </Grid> */}
+
       </div>
 
     </div>
--- src/pages/homePage/lastSection/LastSection.js
+++ src/pages/homePage/lastSection/LastSection.js
@@ -1,41 +1,30 @@
-import { Grid, imageListClasses } from '@mui/material'
+
 import React from 'react'
 import styles from '../../../../style/LastSection.module.css'
-import Image from 'next/image'
 import { skillData } from '../../../component/data/data'
-import image from '../../../assets/image4.png'
 
 
 function LastSection() {
     return (
-        <div>
+    
             <div className={styles.mainContainer}>
-                <div>
                     <h1 className={styles.heading}>Explore Courses</h1>
                     <div className={styles.display1}>
                         {
-                            skillData.map((item,index) => {
+                            skillData.map((item, index) => {
                                 return (
-                            
-                                    <div key={index}>
-                                        <div className={styles.bgColor}>
+                                        <div key={index} className={styles.bgColor}>
                                             <div className={styles.display}>
-                                               <div className={styles.image}> <Image objectFit='fill' height={188} src={image} alt="image not found"/> </div>
-                                                <div><p className={styles.paragraph}>{item.title}</p>
+                                                <div> <img  className={styles.image}src={'/image4.png'} alt="image not found" /> </div>
+                                                <div>
+                                                    <p className={styles.paragraph}>{item.title}</p>
                                                     <p className={styles.paragraph1}>{item.text}</p>
                                                 </div>
                                             </div>
                                         </div>
-                                        </div>
-                                )
-                            })
-                        }
-                </div>
-                </div>
-
-            </div>
+                                )})}
+                    </div>
         </div>
-    )
-}
+    )}
 
 export default LastSection
